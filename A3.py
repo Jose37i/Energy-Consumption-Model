d0d1e6632f253e6a20da946501117d65128ff518
Jose37i <mirandajm@vcu.edu>
2019-10-29 11:10:50 -0400

    all days implemented

--- A3.py
+++ A3.py
@@ -48,6 +48,21 @@ def read_files(normalize):
         return resulting_training_data, resulting_testing_data
 
 
+def read_file(normalize, file_name):
+    resulting_training_data = [[], [], []]
+    resulting_testing_data = [[], [], []]
+    with open(file_name, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for line in csv_reader:
+            resulting_training_data[0].append(1.0)
+            resulting_training_data[1].append(float(line[0]))
+            resulting_training_data[2].append(float(line[1]))
+    if not normalize:
+        return resulting_training_data, resulting_testing_data
+    else:
+        return normalize_it(resulting_training_data)
+
+
 def generate_weights(amount_needed):
     generated_weights = []
     for num in range(amount_needed):
@@ -156,8 +171,12 @@ def make_plot(passed_points, p_weight_set, p_title, p_filename):
     plot.savefig(p_filename)
 
 
-def print_graph_info(neuron_number, p_day_number, p_training_file_name, p_testing_file_name, weights_for_graph, p_train_data, p_test_data):
-    print('Neuron ' + str(neuron_number) + ':')
+def print_graph_info(neuron_number, p_day_number, p_training_file_name, p_testing_file_name, weights_for_graph,
+                     p_train_data, p_test_data):
+    if p_day_number != 0:
+        print('Neuron ' + str(neuron_number) + ' for day ' + str(p_day_number) + ':')
+    else:
+        print('Neuron ' + str(neuron_number) + ':')
     print('\tTraining Graph File Name is: ' + p_training_file_name)
     print('\tTesting Graph File Name is: ' + p_testing_file_name)
     print('\tWeights are [Bias, Hour, Hour^2, Hour^3]  ---> ' + str(weights_for_graph))
@@ -165,39 +184,126 @@ def print_graph_info(neuron_number, p_day_number, p_training_file_name, p_testin
     print('\tTesting Error:' + str(tester(p_test_data, weights_for_graph)) + '\n\n')
 
 
+train_data_day_1 = read_file(True, 'train_data_1.txt')
+train_data_day_2 = read_file(True, 'train_data_2.txt')
+train_data_day_3 = read_file(True, 'train_data_3.txt')
+test_data = read_file(True, 'test_data_4.txt')
+neuron1_weights = generate_weights(2)
+neuron1_weights_day_1 = train(train_data_day_1.copy(), neuron1_weights.copy(), 0.01)
+neuron1_weights_day_2 = train(train_data_day_2.copy(), neuron1_weights.copy(), 0.01)
+neuron1_weights_day_3 = train(train_data_day_3.copy(), neuron1_weights.copy(), 0.01)
+training_title = 'Neuron 1: Training'
+training_file_name = 'Neuron1.Train.'
+testing_tile = 'Neuron 1: Testing'
+testing_file_name = 'Neuron1.Test.'
+make_plot(train_data_day_1, neuron1_weights_day_1, training_title + ' for day 1', training_file_name + 'Day1.png')
+make_plot(test_data, neuron1_weights_day_1, testing_tile + ' for day 1', testing_file_name + 'Day1.png')
+print_graph_info(1, 1, training_file_name + 'Day1.png', testing_file_name + 'Day1.png', neuron1_weights_day_1,
+                 train_data_day_1, test_data)
+
+make_plot(train_data_day_2, neuron1_weights_day_2, training_title + ' for day 2', training_file_name + 'Day2.png')
+make_plot(test_data, neuron1_weights_day_2, testing_tile + ' for day 2', testing_file_name + 'Day2.png')
+print_graph_info(1, 2, training_file_name + 'Day2.png', testing_file_name + 'Day2.png', neuron1_weights_day_2,
+                 train_data_day_2, test_data)
+
+make_plot(train_data_day_3, neuron1_weights_day_3, training_title + ' for day 3', training_file_name + 'Day3.png')
+make_plot(test_data, neuron1_weights_day_3, testing_tile + ' for day 3', testing_file_name + 'Day3.png')
+print_graph_info(1, 3, training_file_name + 'Day3.png', testing_file_name + 'Day3.png', neuron1_weights_day_3,
+                 train_data_day_3, test_data)
+
+neuron2_weights = generate_weights(3)
+neuron2_train_data__day_1 = square_values(train_data_day_1.copy(), 2)
+neuron2_train_data__day_2 = square_values(train_data_day_2.copy(), 2)
+neuron2_train_data__day_3 = square_values(train_data_day_3.copy(), 2)
+test_data_neuron_2 = square_values(test_data.copy(), 2)
+neuron2_weights_day_1 = train(neuron2_train_data__day_1, neuron2_weights.copy(), 0.2)
+neuron2_weights_day_2 = train(neuron2_train_data__day_2, neuron2_weights.copy(), 0.2)
+neuron2_weights_day_3 = train(neuron2_train_data__day_3, neuron2_weights.copy(), 0.2)
+training_title = 'Neuron 2: Training'
+training_file_name = 'Neuron2.Train.'
+testing_tile = 'Neuron 2: Testing'
+testing_file_name = 'Neuron2.Test.'
+
+
+make_plot(neuron2_train_data__day_1, neuron2_weights_day_1, training_title + ' for day 1', training_file_name + 'Day1.png')
+make_plot(test_data_neuron_2, neuron2_weights_day_1, testing_tile + ' for day 1', testing_file_name + 'Day1.png')
+print_graph_info(2, 1, training_file_name + 'Day1.png', testing_file_name + 'Day1.png', neuron2_weights_day_1,
+                 neuron2_train_data__day_1, test_data_neuron_2)
+
+make_plot(neuron2_train_data__day_2, neuron2_weights_day_2, training_title + ' for day 2', training_file_name + 'Day2.png')
+make_plot(test_data_neuron_2, neuron2_weights_day_2, testing_tile + ' for day 2', testing_file_name + 'Day2.png')
+print_graph_info(2, 2, training_file_name + 'Day2.png', testing_file_name + 'Day2.png', neuron2_weights_day_2,
+                 neuron2_train_data__day_2, test_data_neuron_2)
+
+make_plot(neuron2_train_data__day_3, neuron2_weights_day_3, training_title + ' for day 3', training_file_name + 'Day3.png')
+make_plot(test_data_neuron_2, neuron2_weights_day_3, testing_tile + ' for day 3', testing_file_name + 'Day3.png')
+print_graph_info(2, 3, training_file_name + 'Day3.png', testing_file_name + 'Day3.png', neuron2_weights_day_3,
+                 neuron2_train_data__day_3, test_data_neuron_2)
+
+
+neuron3_weights = generate_weights(4)
+neuron3_train_data__day_1 = square_values(neuron2_train_data__day_1.copy(), 3)
+neuron3_train_data__day_2 = square_values(neuron2_train_data__day_2.copy(), 3)
+neuron3_train_data__day_3 = square_values(neuron2_train_data__day_3.copy(), 3)
+test_data_neuron_3 = square_values(test_data_neuron_2.copy(), 3)
+neuron3_weights_day_1 = train(neuron3_train_data__day_1, neuron3_weights.copy(), 0.2)
+neuron3_weights_day_2 = train(neuron3_train_data__day_2, neuron3_weights.copy(), 0.2)
+neuron3_weights_day_3 = train(neuron3_train_data__day_3, neuron3_weights.copy(), 0.2)
+training_title = 'Neuron 3: Training'
+training_file_name = 'Neuron3.Train.'
+testing_tile = 'Neuron 3: Testing'
+testing_file_name = 'Neuron3.Test.'
+
+
+make_plot(neuron3_train_data__day_1, neuron3_weights_day_1, training_title + ' for day 1', training_file_name + 'Day1.png')
+make_plot(test_data_neuron_3, neuron2_weights_day_3, testing_tile + ' for day 1', testing_file_name + 'Day1.png')
+print_graph_info(3, 1, training_file_name + 'Day1.png', testing_file_name + 'Day1.png', neuron3_weights_day_1,
+                 neuron3_train_data__day_1, test_data_neuron_3)
+
+make_plot(neuron3_train_data__day_2, neuron3_weights_day_2, training_title + ' for day 2', training_file_name + 'Day2.png')
+make_plot(test_data_neuron_3, neuron3_weights_day_2, testing_tile + ' for day 2', testing_file_name + 'Day2.png')
+print_graph_info(3, 2, training_file_name + 'Day2.png', testing_file_name + 'Day2.png', neuron3_weights_day_2,
+                 neuron3_train_data__day_2, test_data_neuron_3)
+
+make_plot(neuron3_train_data__day_3, neuron3_weights_day_3, training_title + ' for day 3', training_file_name + 'Day3.png')
+make_plot(test_data_neuron_3, neuron3_weights_day_3, testing_tile + ' for day 3', testing_file_name + 'Day3.png')
+print_graph_info(3, 3, training_file_name + 'Day3.png', testing_file_name + 'Day3.png', neuron3_weights_day_3,
+                 neuron3_train_data__day_3, test_data_neuron_3)
+
+
 train_data, test_data = read_files(True)
 neuron1_weights = generate_weights(2)
 neuron1_weights = train(train_data, neuron1_weights, 0.01)
-training_title = 'Neuron 1: Training'
-training_file_name = 'Neuron1.Train.png'
-testing_tile = 'Neuron 1: Testing'
-testing_file_name = 'Neuron1.Test.png'
+training_title = 'Neuron 1: Training with All Days'
+training_file_name = 'Neuron1.Train.All.Days.png'
+testing_tile = 'Neuron 1: Testing with All Days'
+testing_file_name = 'Neuron1.Test.All.Days.png'
 make_plot(train_data, neuron1_weights, training_title, training_file_name)
 make_plot(test_data, neuron1_weights, testing_tile, testing_file_name)
-print_graph_info(1, 1, training_file_name, testing_file_name, neuron1_weights, train_data, test_data)
+print_graph_info(1, 0, training_file_name, testing_file_name, neuron1_weights, train_data, test_data)
 
 
 neuron2_weights = generate_weights(3)
 train_data_neuron_2 = square_values(train_data, 2)
 test_data_neuron_2 = square_values(test_data, 2)
 neuron2_weights = train(train_data_neuron_2, neuron2_weights, 0.2)
-training_title = 'Neuron 2: Training'
-training_file_name = 'Neuron2.Train.png'
-testing_tile = 'Neuron 2: Testing'
-testing_file_name = 'Neuron2.Test.png'
+training_title = 'Neuron 2: Training with All Days'
+training_file_name = 'Neuron2.Train.All.Days.png'
+testing_tile = 'Neuron 2: Testing with All Days'
+testing_file_name = 'Neuron2.Test.All.Days.png'
 make_plot(train_data_neuron_2, neuron2_weights, training_title, training_file_name)
 make_plot(test_data_neuron_2, neuron2_weights, testing_tile, testing_file_name)
-print_graph_info(2, 1, training_file_name, testing_file_name, neuron2_weights, train_data_neuron_2, test_data_neuron_2)
+print_graph_info(2, 0, training_file_name, testing_file_name, neuron2_weights, train_data_neuron_2, test_data_neuron_2)
 
 
 neuron3_weights = generate_weights(4)
 train3_data = square_values(train_data_neuron_2, 3)
 test_data_neuron_3 = square_values(test_data_neuron_2, 3)
 neuron3_weights = train(train3_data, neuron3_weights, 0.2)
-training_title = 'Neuron 3: Training'
-training_file_name = 'Neuron3.Train.png'
-testing_tile = 'Neuron 3: Testing'
-testing_file_name = 'Neuron3.Test.png'
+training_title = 'Neuron 3: Training with All Days'
+training_file_name = 'Neuron3.Train.All.Days.png'
+testing_tile = 'Neuron 3: Testing with All Days'
+testing_file_name = 'Neuron3.Test.All.Days.png'
 make_plot(train3_data, neuron3_weights, training_title, training_file_name)
 make_plot(test_data_neuron_3, neuron3_weights, testing_tile, testing_file_name)
-print_graph_info(3, 1, training_file_name, testing_file_name, neuron3_weights, train3_data, test_data_neuron_3)
+print_graph_info(3, 0, training_file_name, testing_file_name, neuron3_weights, train3_data, test_data_neuron_3)
